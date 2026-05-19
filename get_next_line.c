6f4125a5792f98abd1edd1ca2f0dc899abe77f28
Andre Brandao Gromicho <abrandao@c1r11s6.42lisboa.com>
2026-05-19 16:06:33 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,88 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abrandao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 12:53:03 by abrandao          #+#    #+#             */
+/*   Updated: 2026/05/19 12:53:09 by abrandao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char    *read_and_store(int fd, char *stored)
+static char	*read_and_store(int fd, char *stored)
 {
-    char    *buffer;
-    int     bytes_read;
+	char	*buffer;
+	int		bytes_read;
 
-    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (!buffer)
-        return (NULL);
-    bytes_read = 1;
-    while (!ft_strchr(stored, '\n') && bytes_read != 0)
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(stored, '\n') && bytes_read != 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(buffer);
+			free(stored);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		stored = ft_strjoin(stored, buffer);
+	}
+	free(buffer);
+	return (stored);
+}
+
+static char	*extract_line(char *stored)
+{
+	char	*line;
+	size_t	i;
+
+	if (!stored || !stored[0])
+		return (NULL);
+	i = 0;
+	while (stored[i] && stored[i] != '\n')
+		i++;
+	if (stored[i] == '\n')
+		i++;
+	line = ft_substr(stored, 0, i);
+	return (line);
+}
+
+static char	*update_stored(char *stored)
+{
+	char	*new_stored;
+	size_t	i;
+
+	if (!stored)
+		return (NULL);
+	i = 0;
+	while (stored[i] && stored[i] != '\n')
+		i++;
+	if (!stored[i] || !stored[i + 1])
+	{
+		free(stored);
+		return (NULL);
+	}
+	new_stored = ft_substr(stored, i + 1, ft_strlen(stored) - i - 1);
+	free(stored);
+	return (new_stored);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*stored;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free (stored);
+		stored = NULL;
+		return (NULL);
+	}
+	stored = read_and_store(fd, stored);
+	if (!stored)
+		return (NULL);
+	line = extract_line(stored);
+	if (!line)
+	{
+		free(stored);
+		stored = NULL;
+		return (NULL);
+	}
+	stored = update_stored(stored);
+	return (line);
+}
+
+/* #include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void)
+{
+    int fd;
+    char *line;
+
+    fd = open("teste.txt", O_RDONLY);
+    if (fd < 0)
     {
-        bytes_read = read(fd, buffer, BUFFER_SIZE);
-        if (bytes_read == -1)
-        {
-            free(buffer);
-            free(stored);
-            return (NULL);
-        }
-        buffer[bytes_read] = '\0';
-        stored = ft_strjoin(stored, buffer);
+        printf("Error opening file");
+        return (1);
     }
-    free(buffer);
-    return (stored);
-}
-
-char    *extract_line(char *stored)
-{
-    char    *line;
-    size_t  i;
-
-    if (!stored || !stored[0])
-        return (NULL);
-    i = 0;
-    while (stored[i] && stored[i] != '\n')
-        i++;
-    if (stored[i] == '\n')
-        i++;
-    line = ft_substr(stored, 0, i);
-    return (line);
-}
-
-char    *update_stored(char *stored)
-{
-    char    *new_stored;
-    size_t  i;
-
-    if (!stored)
-        return (NULL);
-    i = 0;
-    while (stored[i] && stored[i] != '\n')
-        i++;
-    if (!stored[i] || !stored[i + 1])
+    while ((line = get_next_line(fd)) != NULL)
     {
-        free(stored);
-        return (NULL);
-    }   
-    new_stored = ft_substr(stored, i + 1, ft_strlen(stored) - i - 1);
-    free(stored);
-    return (new_stored);
-}
-
-char *get_next_line(int fd)
-{
-    char    *line;
-    static char *stored;
-
-    if (fd < 0 || BUFFER_SIZE <= 0)
-    {
-        free (stored);
-        stored = NULL;
-        return (NULL);
+        printf("line :%s", line);
+        free(line);
     }
-    stored = read_and_store(fd, stored);
-    if (!stored)
-        return (NULL);
-    line = extract_line(stored);
-    if (!line)
-    {
-        free(stored);
-        stored = NULL;
-        return (NULL);
-    }
-    stored = update_stored(stored);
-    return (line);
-}
-
+    close(fd);
+    return (0);
+} */
--- get_next_line.h
+++ get_next_line.h
@@ -7,9 +7,10 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stddef.h>
 
 char	*get_next_line(int fd);
-int     ft_strlen(char *str);
+size_t	ft_strlen(char *str);
 char	*ft_strchr(char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char *s, unsigned int start, size_t len);
